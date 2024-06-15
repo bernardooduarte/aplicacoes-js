68a66e89e45412ba6c86cfbae023328922fe7879
Bernardo Duarte <bernardoduarte.mg@gmail.com>
2024-06-15 15:32:55 -0300

    adicionando aplicação

---
+++ aula-07-operadores/js/index.js
@@ -0,0 +1,9 @@
+var idade, eleitor, resultado;
+idade = 71;
+eleitor = idade < 18 ? "Não eleitor" : "Sim, eleitor";
+
+alert("A resposta é: " + eleitor + " a idade dele é de: " + idade);
+
+resultado = !idade === 65;
+
+alert(resultado);
